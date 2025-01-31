3db7dafbbe64e513b3ef6e679fe25084c2280a32
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-01-31 22:09:54 +0300

    add json-server-auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import Login from "./pages/Login/Login"
 import Home from "./pages/Home/Home"
 import UserProfile from "./pages/UserProfile/UserProfile"
-
-
 import { Route, Routes } from 'react-router-dom';
 import CreateTask from "./pages/CreateTask/CreateTask"
 import Layout from "./layout/Layout"
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App () {
-  
+function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path='/' element={<Layout />}>
+        <Route path='/login' element={<Login />} />
+        
+        <Route path='/' element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }>
           <Route path='' element={<Home />} />
           <Route path='user' element={<UserProfile />} />
           <Route path='create-task' element={<CreateTask />} />
-          </Route>
-
-      <Route path='/login' element={<Login />} />
-
+        </Route>
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
