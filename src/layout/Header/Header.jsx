67259826fa2d28347eb324d52a16d502944f3684
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-17 22:34:13 +0300

    add header stiles

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Header.scss';
+
 
 const Header = () => {
     return (
         <header>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Главная</Link>
-                    </li>
-                    <li>
-                        <Link to="/create-task">Создать задачу</Link>
-                    </li>
-                    <li>
-                        <Link to="/user">Пользователь</Link>
-                    </li>
-                </ul>
+            <div class="header">
+                <div class="header__side">
+                    <div class="header__logo" >W</div>
+                    <div class="header__workspace">
+                        <Link className="texts__title" to="/">WorkTask</Link>
+                    </div>
+                    <div class="header__create-task">
+                        <li>
+                            <Link to="/create-task">Создать задачу</Link>
+                        </li>
+                    </div>
+                </div>
+                <div class="header__side-right">
+                    <div class="header__profile">
+                        <div class="header__user">
+                            <li>
+                                <Link to="/user">Профиль</Link>
+                            </li>
+                            <div class="header__profile.avatar"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
     );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,8 +34,17 @@ const Login = () => {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         },
-        body: JSON.stringify(formData),
+        mode: 'cors',
+        credentials: 'omit',
+        body: JSON.stringify({
+          username: formData.email,
+          password: formData.password
+        }),
       });
 
       if (!response.ok) {
