d0850428f0b1157494648d1707089c43defe8610
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-18 16:25:39 +0300

    add create task button

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -8,14 +8,27 @@ const Header = () => {
         <header>
             <div class="header">
                 <div class="header__side">
-                    <div class="header__logo" >W</div>
                     <div class="header__workspace">
-                        <Link className="texts__title" to="/">WorkTask</Link>
+                        <div class="header-workspace__wrapper">
+                            <div class="header-workspace__tooltip">
+                                <div class="header-workspace__avatar">
+                                <div class="header__logo" >W</div>
+                                </div>
+                                <div class="texts">
+                                    <div class="texts__description">
+                                        <div class="texts__name">Проект</div>
+                                    </div>
+                                    <Link class="texts__title" to="/">WorkTask</Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="header__create-task">
-                        <li>
-                            <Link to="/create-task">Создать задачу</Link>
-                        </li>
+                    <div class="change-service">
+                        <div class="change-service__inner">
+                            <div class="change-service__title">
+                                <Link class="texts__title" to="/create-task">Создать задачу</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="header__side-right">
@@ -34,3 +47,9 @@ const Header = () => {
 };
 
 export default Header;
+
+// <div class="header__create-task">
+// <li>
+//     <Link to="/create-task">Создать задачу</Link>
+// </li>
+// </div>
