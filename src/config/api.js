530ca1241a9f62fbfc40d88c025650f1a17711a7
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-21 00:17:33 +0300

    fix urls in env

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,9 @@ export const BASE_URL = process.env.REACT_APP_BASE_URL || '/work-task/v1';
 
 export const API_ENDPOINTS = {
   LOGIN: `${API_URL}${BASE_URL}/login`,
-  REGISTER: `${BASE_URL}/work-task/v1/register`,
-  LOGOUT: `${BASE_URL}/work-task/v1/logout`,
-  REFRESH: `${BASE_URL}/work-task/v1/refresh`,
-  USER_INFO: `${BASE_URL}/work-task/v1/user-info`,
+  REGISTER: `${API_URL}/register`,
+  LOGOUT: `${API_URL}/logout`,
+  REFRESH: `${API_URL}/refresh`,
+  USER_INFO: `${API_URL}${BASE_URL}/user-info`,
+  CREATE_TASK: `${API_URL}${BASE_URL}/task/createTask`,
 }; 
