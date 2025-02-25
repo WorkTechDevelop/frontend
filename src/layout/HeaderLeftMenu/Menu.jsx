47fb2761a358913aea455053fcadcfbec84bf092
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-02-25 21:02:19 +0300

    fix page content in pages

--- src/layout/HeaderLeftMenu/Menu.jsx
+++ src/layout/HeaderLeftMenu/Menu.jsx
@@ -4,26 +4,18 @@ import './Menu.scss';
 
 const Menu = () => {
     return (
-        <div className="left-menu__container">
+        <div className="">
             <nav>
                 <div className="menu__container">
-                    <ul>
-                        <div className="main__container">
-                            <icon className="main__icon"></icon>
-                            <li><Link to="/">Главная</Link></li>
-                        </div>
-                        <div className="task__container">
-                            <icons className="task__icon"></icons>
-                            <li><Link to="/tasks">Задачи</Link></li>
-                        </div>
-                        <div className="members__container">
-                            <icon className="member__icon"></icon>
-                            <li><Link to="/projects-member">Участники</Link></li>
-                        </div>
-                        <div className="settings-project__container">
-                            <icon className="settings-project__icon"></icon>
-                            <li><Link to="/projects-settings">Настройки</Link></li>
-                        </div>
+                    <ul className="menu-list">
+                        <icon className="main__icon"></icon>
+                        <li><Link to="/">Главная</Link></li>
+                        <icons className="task__icon"></icons>
+                        <li><Link to="/tasks">Задачи</Link></li>
+                        <icon className="member__icon"></icon>
+                        <li><Link to="/projects-member">Участники</Link></li>
+                        <icon className="settings-project__icon"></icon>
+                        <li><Link to="/projects-settings">Настройки</Link></li>
                     </ul>
                 </div>
             </nav>
