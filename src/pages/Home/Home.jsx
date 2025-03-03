38ea4d102133d6693a93f6a4daeb5fabe45bf572
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-03-04 00:37:50 +0300

    add base jsx and create scss style home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Главная страница
+        <div className="home">
+            <div className="project-name__title">
+                <p>Наименование проекта</p>
+                <button className='add-progect__icon'></button>
+            </div>
         </div>
     )
 }
