c6ecde1e0ef645b6681db5aa667a2f5556fc177a
krutakov <krutakovegor43@gmail.com>
2025-07-17 13:07:48 +0300

    Refactor useUpdateTask to accept projectId and taskId parameters, and transform API response using mapTaskDataDtoToTask for improved data handling.

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -3,22 +3,23 @@ import { client } from "@/lib/rpc";
 import { UpdateTaskModelDTO } from "@/lib/types.api";
 import { Task } from "../types";
 import { toast } from "sonner";
+import { mapTaskDataDtoToTask } from "./utils";
 
 type RequestType = UpdateTaskModelDTO;
 type ResponseType = Task;
 
-export const useUpdateTask = () => {
+export const useUpdateTask = (projectId: string, taskId: string) => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async (json: RequestType) => {
-      const response = await client.updateTask(json);
-      return response;
+      const response = await client.updateTask(projectId, taskId, json);
+      return mapTaskDataDtoToTask(response);
     },
     onSuccess: () => {
       toast.success("Задача обновлена успешно");
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
-      queryClient.invalidateQueries({ queryKey: ["task", undefined] });
+      queryClient.invalidateQueries({ queryKey: ["projects"] });
     },
     onError: (error: Error) => {
       toast.error(error.message || "Ошибка обновления задачи");
