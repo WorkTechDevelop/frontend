8e487d94f747813e5f8e8a25004061e269f09921
egorKru <krutakovegor43@gmail.com>
2025-07-25 05:15:17 +0300

    Update query client configuration in QueryProvider component
    - Increased staleTime to 5 minutes and gcTime to 10 minutes for improved data management.
    - Disabled refetching on window focus and reconnect to enhance performance.
    - Set retry attempts to 1 for query failures, optimizing the query behavior.

--- src/components/layout/query-provider.tsx
+++ src/components/layout/query-provider.tsx
@@ -10,7 +10,11 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 5 * 60 * 1000, // 5 минут
+        gcTime: 10 * 60 * 1000, // 10 минут
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        retry: 1,
       },
     },
   });
