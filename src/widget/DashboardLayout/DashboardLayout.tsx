999bc1f4178d8650d21250814477a185519f5b22
Grigorii Veinin <kage.grifon@yandex.ru>
2025-08-29 21:25:17 +0500

    feat: add mui and top dashboad part

--- src/widget/DashboardLayout/DashboardLayout.tsx
+++ src/widget/DashboardLayout/DashboardLayout.tsx
@@ -1,8 +1,72 @@
-// import { CreateTaskModal } from "@/features/tasks/components/create-task-modal";
-// import { CreateProjectModal } from "@/features/projects/components/create-project-modal";
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add'
+import Avatar from '@mui/material/Avatar'
 
-// import { Navbar } from "@/components/layout/navbar";
-// import { Sidebar } from "@/components/layout/sidebar";
+import styled from '@emotion/styled'
+
+const leftSideWidthPx = '224px'
+const blockBorderWidthPx = '3px'
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  border: 1px solid rgba(0, 0, 0, 1);
+  border-radius: 30px;
+  background-color: rgba(255, 255, 255, 1);
+  overflow: hidden;
+
+  gap: ${blockBorderWidthPx};
+`
+
+const TopBlock = styled.header`
+  height: 120px;
+  width: 100%;
+  display: flex;
+  gap: ${blockBorderWidthPx};
+`
+
+const HeaderSideBlock = styled.div`
+  width: ${leftSideWidthPx};
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(224, 228, 234, 1);
+`
+const HeaderMainBlock = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding-left: 60px;
+  padding-right: 20px;
+  gap: 16px;
+
+  background-color: rgba(224, 228, 234, 1);
+`
+
+const MainBlock = styled.div`
+  display: flex;
+  gap: ${blockBorderWidthPx};
+`
+
+const ContentBlock = styled.main`
+  flex-grow: 1;
+  background-color: rgba(224, 228, 234, 1);
+`
+
+const Spacer = styled.div`
+  flex-grow: 1;
+`
+
+const Sidebar = styled.aside`
+  width: ${leftSideWidthPx};
+
+  flex-shrink: 0;
+  background-color: rgba(224, 228, 234, 1);
+`
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -10,21 +74,71 @@ interface DashboardLayoutProps {
 
 export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <div className="min-h-screen">
-      {/* <CreateProjectModal />
-      <CreateTaskModal /> */}
+    <PageWrapper>
+      <TopBlock>
+        <HeaderSideBlock>
+          <WorkTaskLogo />
+        </HeaderSideBlock>
+        <HeaderMainBlock>
+          <CreateNewTaskButton />
+          <ProjectName />
+          <Spacer />
+          <UserProfile />
+        </HeaderMainBlock>
+      </TopBlock>
+      <MainBlock>
+        <Sidebar />
+        <ContentBlock>{children}</ContentBlock>
+      </MainBlock>
+    </PageWrapper>
+  )
+}
 
-      <div className="flex w-full h-full">
-        <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
-          {/* <Sidebar /> */}
-        </div>
-        <div className="lg:pl-[264px] w-full">
-          <div className="mx-auto max-w-screen-2xl h-full">
-            {/* <Navbar /> */}
-            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
-          </div>
-        </div>
-      </div>
+function WorkTaskLogo() {
+  return (
+    <div
+      style={{
+        color: 'rgba(98, 0, 255, 1)',
+        fontSize: '35px',
+        fontWeight: 200,
+      }}
+    >
+      WorkTask
     </div>
+  )
+}
+
+const PurpleButton = styled(Button)({
+  backgroundColor: 'rgba(80, 48, 229, 0.73)',
+  borderColor: 'rgba(80, 48, 229, 0.73)',
+})
+
+function CreateNewTaskButton() {
+  return (
+    <PurpleButton variant="contained" startIcon={<AddIcon />}>
+      New task
+    </PurpleButton>
+  )
+}
+
+function ProjectName() {
+  return (
+    <div
+      style={{
+        color: 'rgba(13, 6, 45, 1)',
+        fontSize: '35px',
+        fontWeight: 500,
+      }}
+    >
+      WorkTask
+    </div>
+  )
+}
+
+function UserProfile() {
+  return (
+    <Avatar sx={{ bgcolor: 'rgba(98, 0, 255, 1)' }} alt="Grigorii Veinin">
+      GV
+    </Avatar>
   )
 }
